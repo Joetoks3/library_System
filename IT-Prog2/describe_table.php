b37c162d792add3621424c3510fa49cf86882eef
Joetoks3 <2092515@g.cu.edu.ph>
2026-03-16 00:57:29 +0800

    Delete IT-Prog2 directory

--- IT-Prog2/describe_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connection.php");
-
-$result = mysqli_query($con, "DESCRIBE borrowed_books");
-while($row = mysqli_fetch_assoc($result)) {
-    echo $row['Field'] . ' - ' . $row['Type'] . '<br>';
-}
-?>
